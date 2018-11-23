4ca1c35f88b4c4625d0676d8990018f6e336ecf9
Medicine Yeh <medicinehy@gmail.com>
2018-11-23 12:38:41 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@
 (cd axiom-sim && docker build -t medicineyeh/axiom-sim .)
 # Build the second container which is based on the axiom-sim
 (cd axiom-dev && docker build -t medicineyeh/axiom-dev .)
+
+# Build the QEMU container
+(cd axiom-qemu && ./build.sh)
+# Build the second container which is based on the axiom-sim
+(cd axiom-pkgbuild && docker build -t medicineyeh/axiom-pkgbuild .)
